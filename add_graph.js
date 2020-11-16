aa6f869d2c1a2a2478994d411df943070896d4b0
CFieldhouse <timpat1111@gmail.com>
2020-11-16 23:38:36 +0000

    button and graph now at top of page

--- add_graph.js
+++ add_graph.js
@@ -1,11 +1,10 @@
 function embed_graph(){
 	var title = document.getElementById("firstHeading").innerHTML
-	trends.embed.renderExploreWidget("TIMESERIES", {"comparisonItem":[{"keyword":"sierra leone","geo":"","time":"today 5-y"}],"category":0,"property":""}, {"exploreQuery":"date=today%205-y&q=sierra%20leone","guestPath":"https://trends.google.com:443/trends/embed/"});
+	var first_heading = document.getElementById("firstHeading");
+	trends.embed.renderExploreWidgetTo(first_heading, "TIMESERIES", {"comparisonItem":[{"keyword":title,"geo":"","time":"today 5-y"}],"category":0,"property":""}, {"exploreQuery":"date=today%205-y&q=sierra%20leone","guestPath":"https://trends.google.com:443/trends/embed/"});
 }
 var button = document.createElement("button");
 button.onclick=embed_graph;
-button.innerHTML = "Trends";
-document.body.appendChild(button);
+button.innerHTML = "Google Trends";
+document.getElementById("bodyContent").prepend(button);
 
-
-trends.embed.renderExploreWidget("TIMESERIES", {"comparisonItem":[{"keyword":"sierra leone","geo":"","time":"today 5-y"}],"category":0,"property":""}, {"exploreQuery":"date=today%205-y&q=sierra%20leone","guestPath":"https://trends.google.com:443/trends/embed/"});
